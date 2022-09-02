7b4df7ca7983cf89dd0560d160c6b1f00d77d332
Anderson Florencio <anderson.florencio@sti.ufc.br>
2022-09-02 10:44:23 -0300

    first commit

---
+++ ansible-lab/control-node/provision.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Ansible install start..."
+sudo yum -y install epel-release
+sudo yum -y install ansible
+cat <<EOT >> /etc/hosts
+192.168.1.2 control-node
+192.168.1.3 app01
+192.168.1.4 db01
+EOT
---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+cat <<EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0EG7QUsdVgF0lyyHQTKEiPN8VJGjcx30LOgZ8ly9jA+ljupHpVNgxLrVMnL7Mc6T6kE1nU7ph3U2yrDvXAcXxxv7fB3Kz+nlmTzQnd7AmuOz46IiuaI5mHoSe87XfF6rvoP2lBS6gvj/hfDXaiKlNugHNkGz5YHP/FvPlm7IufKJPNwcndxFKd2qRUonFuhbbr6MWzkCoM/UJyfvveg+ElyjW6FubDjp6dCA3NuGbwR7tzgweSp0p8nbJHLLJi0MCMPUn+9uHflpOzHqf1Pq5YzMqnpGG5S7G7AHiQmD/FfYzZ6rtjp1lmbRqfXC2vWaSVvakt/J3tpnj29eHrgQF
+EOT
